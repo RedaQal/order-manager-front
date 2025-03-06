1c857cd2c80387baa53d7bf4cfb475e7eb18ce87
abdel-kobbi <mr.abdelkhaliqkobbi@gmail.com>
2025-03-06 01:11:10 +0000

    add Details order component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import DetailsOrder from './components/DetailsOrder'
 import "./App.css"
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/details-order/:id' element={<DetailsOrder />} />
+      </Routes>
     </>
   )
 }
