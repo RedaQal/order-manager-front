3bc99e01bb9308d7882def0384621c54a14e5994
QALQOL Reda <redaqal@gmail.com>
2025-03-06 00:56:32 +0000

    adding the application header

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+export default function Header() {
+  const location = useLocation();
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand ps-4" to="/">Order Manager</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav m-auto">
+            <li className="nav-item active">
+              <Link className={`nav-link link ${location.pathname === "/list-orders" ? "active" : ""}`} to={"/list-orders"}>List orders</Link>
+            </li>
+          </ul>
+        </div>
+      </nav >
+    </>
+  )
+}
